318a6cf4c2c666d93cf0a109311474c5d078b6fc
Marekmp@outlook.sk <marekpavlik8@azet.sk>
2021-11-29 19:34:49 +0100

    dokoncene

--- Open-Lab-01.04/Triangle.cs
+++ Open-Lab-01.04/Triangle.cs
@@ -6,7 +6,9 @@ namespace Open_Lab_01._04
     {
         public double Area(double foundation, double height)
         {
-            throw new NotImplementedException();
+            
+            
+            return (foundation * height) / 2;
         }
     }
 }
